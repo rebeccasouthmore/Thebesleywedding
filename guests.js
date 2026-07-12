12a978f11c4bd03e79494c3e483e8c138e8094dc
rebeccasouthmore <rebeccasouthmore@gmail.com>
2026-07-12 09:19:21 +0100

    Add guest database structure

---
+++ guests.js
@@ -0,0 +1,30 @@
+
+/*
+Project Besley
+Guest Database
+Rebecca and Matthew
+*/
+
+const guests = [
+
+    {
+        code: "B001",
+        names: [
+            "Mr & Mrs Smith"
+        ],
+        childrenInvited: false
+    },
+
+
+    {
+        code: "B002",
+        names: [
+            "The Jones Family",
+            "Emily Jones",
+            "Oliver Jones"
+        ],
+        childrenInvited: true
+    }
+
+];
+
